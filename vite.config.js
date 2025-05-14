04662af8fdca4a5daa50c03d73df77381032bb1e
todoapp-pablo <sfsolomid10@gmail.com>
2025-05-14 08:56:08 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/taskList/", // 👈 ОБЯЗАТЕЛЬНО
   plugins: [react()],
 });
