9aee68387eee21939161e6a566fc9e49aa7f1873
todoapp-pablo <sfsolomid10@gmail.com>
2025-05-14 08:48:38 +0200

    up

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/taskList/", // 👈 ОБЯЗАТЕЛЬНО
   plugins: [react()],
-})
+});
